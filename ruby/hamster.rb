000a1f385d9f457e729dd9a46f8cae282b4aa8da
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-02 14:22:21 -0700

    changing the code to collect the right type of data

--- ruby/hamster.rb
+++ ruby/hamster.rb
@@ -8,13 +8,20 @@ puts "What is the hamsters name?"
 name = gets.chomp
 
 puts "On a scale of 1 to 10 rank the volume of the hamster"
-volume = gets.chomp
+volume = gets.chomp.to_i
 
 puts "What is the fur color?"
 color = gets.chomp
 
-puts "Is the hamster a good candidate for adoption?(answer true or false)"
-adoption = gets.chomp
+puts "Is the hamster a good candidate for adoption?(answer yes or no)"
+answer = gets.chomp
 
-puts "How old is the hamster?"
-age = gets.chomp
+if answer == "yes"
+  adoption = true
+elsif answer =="no"
+  adoption = false
+end
+
+puts "How old is the hamster?(years to the nearest month)"
+age = gets.chomp.to_f
+
