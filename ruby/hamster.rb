84f4ea660b412c95309e2eb06df4e79ff87eb83c
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-02 14:14:28 -0700

    asked the 5 questions in ruby code

---
+++ ruby/hamster.rb
@@ -0,0 +1,20 @@
+# ask for the hamsters name
+# volume level
+# determine color
+# is a good candidate for adoption
+# age
+
+puts "What is the hamsters name?"
+name = gets.chomp
+
+puts "On a scale of 1 to 10 rank the volume of the hamster"
+volume = gets.chomp
+
+puts "What is the fur color?"
+color = gets.chomp
+
+puts "Is the hamster a good candidate for adoption?(answer true or false)"
+adoption = gets.chomp
+
+puts "How old is the hamster?"
+age = gets.chomp
