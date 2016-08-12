79223fac251301f0d170de4d0fe1d7b382fcd2aa
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-11 17:30:59 -0700

    practice each and map on both hash and arrays

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -7,5 +7,27 @@ end
 
 block_test{|food1, food2|puts "I would like to eat some #{food1}, #{food2}"}
 
+#Release 1
+cats = ["tabby", "calico", "siamese"]
+dogs = {retriever: "golden", husky: "white"}
 
+p cats
+cats.each do |cat|
+ p cat.capitalize
+end
+p cats
 
+p dogs
+dogs.each do |breed, color|
+ puts "#{breed} has a #{color} coat"
+end 
+p dogs
+
+p cats
+cats.map! do |cat|
+ cat.capitalize
+end
+p cats
+
+# Testing syntax
+# cats.each { |cat| p cat.capitalize }
