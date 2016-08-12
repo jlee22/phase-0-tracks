9abc5a9f35eda7f9167f55837fb3afa8e4458c13
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-11 17:32:33 -0700

    adding release 2 and practice new methods to complete the filtering tasks

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -31,3 +31,39 @@ p cats
 
 # Testing syntax
 # cats.each { |cat| p cat.capitalize }
+
+#release 2
+
+numbers = [1,2,3,4,7,8,9,10,12]
+hash = {a: 1, b: 2, c: 3, d: 4, e: 5}
+
+# 1
+numbers.delete_if{ |number| number>10}
+p numbers
+
+hash.delete_if {|letter,number| letter >= :d}
+p hash
+
+# 2
+numbers.select! { |number| number.even?}
+p numbers
+
+hash.select! { |letter,number| number.odd?}
+p hash
+
+# 3
+numbers.keep_if {|number| number<6}
+p numbers
+
+hash.keep_if { |letter, number| letter == :a}
+p hash
+
+# 4
+new_numbers = [1, 2, 3, 4, 5, 0]
+other_numbers = new_numbers.take_while { |digit| digit < 4 } 
+p other_numbers
+
+
+# trying another method???? both complete the required task?
+# more_numbers = other_numbers.drop_while { |digit| digit < 2 } 
+# p more_numbers
