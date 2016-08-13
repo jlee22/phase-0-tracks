bdd5da4e1c990126ee7db6956403f28206283b3d
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-12 21:13:05 -0700

    the program will now store aliases in array and making adjustments to account for invalid entries

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -14,11 +14,11 @@ def alias_disguise (name)
 	first_name_array = name_array[1].downcase.split('') 
 
 	last_name_array.each do |last_name|
-        last_name.to_s.tr!('abcdefghijklmnopqrstuvwxyz','ecdfighjoklmnpuqrstvawxyzb')
+        last_name.to_s.tr!('abcdefghijklmnopqrstuvwxyz ','ecdfighjoklmnpuqrstvawxyzb ')
     end
 
     first_name_array.each do |first_name|
-        first_name.to_s.tr!('abcdefghijklmnopqrstuvwxyz','ecdfighjoklmnpuqrstvawxyzb')
+        first_name.to_s.tr!('abcdefghijklmnopqrstuvwxyz ','ecdfighjoklmnpuqrstvawxyzb ')
     end
 
     disguised_last = last_name_array
@@ -30,21 +30,27 @@ end
 
 p alias_disguise("Felicia Torres")	
 
-
+agent_list = []
 
 puts "Please enter first and last name to disguise. type \"quit\" to quit"
 agent_name = gets.chomp
 
 until agent_name == "quit"
-	if agent_name == ""
+	if agent_name == ""	
 		puts "Enter another pair of first and last name to disguise. type \"quit\" to quit"
 		agent_name = gets.chomp
-	else
+	elsif agent_name.include?(" ")
 		p alias_disguise(agent_name)
+		agent_list << "#{alias_disguise(agent_name)} is actually #{agent_name}"
+		puts "Enter another pair of first and last name to disguise. type \"quit\" to quit"
+		agent_name = gets.chomp
+	else 
 		puts "Enter another pair of first and last name to disguise. type \"quit\" to quit"
 		agent_name = gets.chomp
 	end
 end
+
+p agent_list
 
 
 
