3d87c9082027f8ca5a2e8193edb462949cd90a4d
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-16 17:31:35 -0700

    adding new methods for puppy class and driver code at bottom to make sure they all work

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -2,8 +2,32 @@ class Puppy
 
   def fetch(toy)
     puts "I brought back the #{toy}!"
-    toy
   end
 
+  def speak(integer) 
+  integer.to_i.times {puts "Woof!"}
+  end 	
+  
+  def roll_over
+  puts "*rolls over*"
+  end
+
+  def dog_years(age)
+  age.to_i * 7
+  end
+
+  def high_five(string)
+	  puts "raises #{string} paw"
+  end
+  
 end
 
+
+Husky = Puppy.new
+
+
+Husky.fetch("ball")
+Husky.speak(3)
+Husky.roll_over
+p Husky.dog_years(5)
+Husky.high_five("left")
