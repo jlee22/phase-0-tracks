95d6310a212625889e246f13db844fa8d9961905
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-16 20:07:42 -0700

    adding and testing release 2 (new methods, new getter methods)

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,5 +1,13 @@
 class Santa
 
+	def initialize(gender, ethnicity)
+		@gender = gender
+		@ethnicity = ethnicity
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", 
+						"Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
+	end
+
 	def speak
 		puts "Ho, ho, ho! Haaappy holidays!"
 	end
@@ -8,13 +16,69 @@ class Santa
 		puts "That was a good #{cookie_type}!"
 	end
 
-	def initialize
-		puts "Initializing Santa instance..."
+	def celebrate_birthday
+		@age += 1
 	end
 
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		@reindeer_ranking << reindeer_name
+	end
+
+	def about
+		puts "Gender: #{@gender}"
+		puts "Ethnicity: #{@ethnicity}"
+		puts "Reindeer Ranking: #{reindeer_ranking}"
+		puts "Age: #{age}"
+	end
+
+	#getter methods for attributes
+	def gender
+		@gender
+	end
+
+	def ethnicity
+		@ethnicity
+	end
+
+	def reindeer_ranking
+		@reindeer_ranking
+	end
+
+	def age
+		@age
+	end
+	#setter methods
+	
 end
 
-Bob = Santa.new
+# Testing release 2
+santa = Santa.new("male","asian")
+santa.get_mad_at("Vixen")
+santa.celebrate_birthday
+santa.about
 
-Bob.speak
-Bob.eat_milk_and_cookies("chocolate cookie")
+
+
+# Testing release 0
+# Bob = Santa.new
+# Bob.speak
+# Bob.eat_milk_and_cookies("chocolate cookie")
+
+
+# santas = []
+# santas << Santa.new("agender", "black")
+# santas << Santa.new("female", "Latino")
+# santas << Santa.new("bigender", "white")
+# santas << Santa.new("male", "Japanese")
+# santas << Santa.new("female", "prefer not to say")
+# santas << Santa.new("gender fluid", "Mystical Creature (unicorn)")
+# santas << Santa.new("N/A", "N/A")
+
+# santas = []
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African",
+#  "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_genders.length.times do |i|
+#   santas << Santa.new(example_genders[i], example_ethnicities[i])
+# end
