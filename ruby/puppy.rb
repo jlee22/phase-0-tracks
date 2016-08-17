04a59199cfbb8c8755b862228d817504ca2ca74c
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-16 17:03:42 -0700

    adding puppy class design as comments

---
+++ ruby/puppy.rb
@@ -0,0 +1,16 @@
+# SPECIES ------------------------
+# Yappy Fluffy
+
+# CHARACTERISTICS ----------------
+# Tail: fluffy
+# Fur length: short
+# Friendly: yes
+# Size: medium
+# Name: varies
+# Color: white
+
+# BEHAVIOR -----------------------
+# Barks
+# Hug
+# Fetch
+# High-Fives
