dabb46e9bc68f7a9565ab39ab0305dc3d2f4d3e4
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-17 18:46:15 -0700

    updated flight to include class and mixins, updated shout.rb to have mixins as well and made two new classes with driver code

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,13 +1,40 @@
 
+# module Shout
+#   def self.yell_angrily(words)
+#     words + "!!!" + " :("
+#   end
+
+#   def self.yelling_happily(words)
+#   	words + "!!!" + " :D"
+#   end
+# end
+
+# p Shout.yell_angrily("wow")
+# p Shout.yelling_happily("wooo")
+
 module Shout
-  def self.yell_angrily(words)
-    words + "!!!" + " :("
+ def yell_angrily(words)
+    p words.capitalize + "!!!" + " :("
   end
 
-  def self.yelling_happily(words)
-  	words + "!!!" + " :D"
+  def yelling_happily(words)
+  	p words.capitalize + "!!!" + " :D"
   end
 end
 
-p Shout.yell_angrily("wow")
-p Shout.yelling_happily("wooo")
+
+class Parents
+	include Shout
+end
+
+class Police
+	include Shout
+end
+
+mom = Parents.new
+mom.yell_angrily("do your homework")
+mom.yelling_happily("thanks for helping out")
+
+policeman = Police.new
+policeman.yell_angrily("freeze")
+policeman.yelling_happily("donuts")
