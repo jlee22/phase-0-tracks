bee32f893bbe6244cbcd0287fb12deec52b24e4e
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-25 15:12:13 -0700

    release 0: made two new arrays of color and horse names

---
+++ js/data_structures.js
@@ -0,0 +1,9 @@
+var color = ["blue", "red", "yellow", "orange"]
+var horse_name = ["Ed", "John", "Max", "George"]
+
+color.push("green")
+horse_name.push("Joe")
+
+//driver code
+console.log(color)
+console.log(horse_name)
