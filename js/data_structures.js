5c7b82e9f02815d150533bf6e492605a8b641fd9
Jeffrey Lee <jfflee.22@gmail.com>
2016-08-25 15:27:24 -0700

    release 1: we made the combine array function that made a hash-like object with horse names and color

--- js/data_structures.js
+++ js/data_structures.js
@@ -4,6 +4,20 @@ var horse_name = ["Ed", "John", "Max", "George"]
 color.push("green")
 horse_name.push("Joe")
 
+//create a hash-like object using the two arrays provided
+//ex: {'Ed':'blue'....}
+
+
+
+function combine_array(array1,array2) {
+	var hash_new={}
+	for (i=0;i < array1.length; i++) {
+	hash_new[array1[i]] = array2[i];
+	}
+return hash_new;
+}
+
 //driver code
-console.log(color)
-console.log(horse_name)
+// console.log(color)
+// console.log(horse_name)
+console.log(combine_array(horse_name,color))
