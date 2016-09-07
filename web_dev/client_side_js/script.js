311940551507d5c417148197ab4a8d255531e081
Jeffrey Lee <jfflee.22@gmail.com>
2016-09-07 16:29:27 -0700

    release 2: adding event listeners and adding tennis gif that can be revealed by mouseover

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -5,6 +5,7 @@ var photo = document.getElementById("tennis-photo");
 photo.style.border = "5px solid orange";
 photo.style.padding = "5px";
 
+var gif = document.getElementById("tennis-gif").style.visibility = "hidden";
 
 // var list = document.getElementById("list");
 // list.firstElementChild.style.color = "red";
@@ -19,3 +20,36 @@ for (i = 0; i < ul.children.length ; i++) {
 ul.children[i].style.padding = "3px";
 ul.children[i].style.color = "orange";
 }
+
+function changecolor(event) {
+	event.target.style.color = "blue"
+}
+
+function changecolor2(event) {
+	event.target.style.color = "black"
+}
+
+
+function hide(event) {
+	document.getElementById("tennis-gif").style.visibility = "hidden";
+}
+
+function reveal(event) {
+	document.getElementById("tennis-gif").style.visibility = "visible";
+}
+
+var ol = document.getElementById("list");
+ol.addEventListener("mouseover", changecolor);
+
+var ol = document.getElementById("list");
+ol.addEventListener("mouseout", changecolor2);
+
+var h2 = document.getElementById("h2-gif");
+h2.addEventListener("mouseover", reveal);
+
+var ol = document.getElementById("h2-gif");
+ol.addEventListener("mouseout", hide);
+
+
+
+
