453f68620d08fc9dc6e7a3ea7f13e074c2421c94
Jeffrey Lee <jfflee.22@gmail.com>
2016-09-09 14:06:14 -0700

    Adding a review erb and form to allow user to add comments into the review database, also made review.db

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -1,11 +1,16 @@
 # require gems
 require 'sinatra'
+require "sinatra/reloader" if development?
 require 'sqlite3'
+
 
 set :public_folder, File.dirname(__FILE__) + '/static'
 
 db = SQLite3::Database.new("students.db")
 db.results_as_hash = true
+review_db = SQLite3::Database.new("reviews.db")
+review_db.results_as_hash = true
+
 
 # show students on the home page
 get '/' do
@@ -17,11 +22,28 @@ get '/students/new' do
   erb :new_student
 end
 
+
+# get '/review' do
+# 	@students = db.execute("SELECT * FROM students")
+# 	erb :new_template
+# end
+
+get '/students/review/form' do
+	erb :review_form
+end
+
+post '/reviews' do
+ 	review_db.execute("INSERT INTO reviews (comment) VALUES (?)", [params['comment']])
+  	redirect '/review'
+end
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+
 
 # add static resources
